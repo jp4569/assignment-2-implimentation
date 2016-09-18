71de154f022c290e38ad977d793a2fbd1856c930
jp4569 <jpopovic@albany.edu>
2016-09-18 19:49:53 -0400

    fixed examine

--- rpg game for assignment 2/src/Food.java
+++ rpg game for assignment 2/src/Food.java
@@ -14,7 +14,7 @@ public void setNutrition(int nutrition){
 public void setQuantity(int quantity){
 	this.quantity = quantity;
 }
-public void examine3(){
+public void examine4(){
 	System.out.println("Name: " + getName());
 	System.out.println("Name: " + getWeight());
 	System.out.println("Nutrition: " + nutrition);
