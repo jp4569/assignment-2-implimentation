4afb597f1fdb5b8ef0c8fd7c69ccdde5b094a7a3
jp4569 <jpopovic@albany.edu>
2016-09-18 19:49:07 -0400

    added objects for main class

--- rpg game for assignment 2/src/Main.java
+++ rpg game for assignment 2/src/Main.java
@@ -4,8 +4,15 @@ public class Main{
 		 Item impliment = new Item();
 		 Weapon impliment2 = new Weapon();
 		 Armor impliment3 = new Armor();
+		 Food impliment4 = new Food();
 		 impliment.setName("Jak Popovic");
 		 impliment.setWeight(200);
+		 impliment.examine();
+		 impliment2.setDamage(4500);
+		 impliment2.examine2();
+		 impliment3.setDefense(5500);
+		 impliment3.examine3();
+		 impliment4.examine4();
 		 
 	}
 
