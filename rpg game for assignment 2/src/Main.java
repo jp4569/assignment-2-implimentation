dbc1fc03ffc74ad100ce494fb4d55ec4c48dc530
jp4569 <jpopovic@albany.edu>
2016-09-18 13:04:18 -0400

    begins to impliment

--- rpg game for assignment 2/src/Main.java
+++ rpg game for assignment 2/src/Main.java
@@ -1,8 +1,12 @@
 
-public class Main {
+public class Main{
 	 public static void main(String[] args){
 		 Item impliment = new Item();
-		
+		 Weapon impliment2 = new Weapon();
+		 Armor impliment3 = new Armor();
+		 impliment.setName("Jak Popovic");
+		 impliment.setWeight(200);
+		 
 	}
 
 }
